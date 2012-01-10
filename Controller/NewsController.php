f9def45e076430f9595dc48c879936398824f740
Matthew Dunham <me@dunhamzzz.com>
2012-01-10 13:30:43 +0000

    SImplifying news posting logic

--- Controller/NewsController.php
+++ Controller/NewsController.php
@@ -1,58 +1,66 @@
 <?php
-class NewsController extends AppController {
-	
-	public function view($slug) {
-		
-		$news = $this->News->find('first', array(
-			'conditions' => array(
-				'News.slug' => $slug,
-				'News.deleted' => 0,
-				'News.published' => 1
-			),
-			'contain' => array(
-				'User' => array('username', 'slug')
-			)
-		));
-		
-		if(!$news) {
-			throw new NotFoundException('Invalid News Item');
-		}
-		
-		// Get product
-		$product = $this->News->Product->getById($news['News']['product_id']);
-		
-		$title_for_layout = $news['News']['title'];
-		$this->set(compact('title_for_layout', 'news', 'product'));
-	}
-	
-	public function submit($productSlug) {
-		$product = $this->News->Product->getBySlug($productSlug);
-		
-		if(!$product) {
-			throw new NotFoundException(__('Invalid Product'));
-		}
-		
-		if($this->request->is('post')) {
-			
-			$this->request->data['News']['product_id'] = $product['Product']['id'];
-			
-			try {
-				$news = $this->News->submit($this->request->data, AuthComponent::user('id'));
-			} catch(Exception $e) {
-				$this->setFlash($e->getMessage);
-			}
 
-			if($news !== false) {
-				$this->Session->setFlash(__('Thanks for submiting your %s news.', htmlspecialchars($product['Product']['name'])));
-				$this->redirect(array('controller' => 'news', 'action' => 'view', 'newsSlug' => $news['News']['slug']));
-			}
-		}
-		
-		// For some reason the invalid fields won't register unless I do this
-		$this->News->invalidFields();
-		
-		$title_for_layout = __('Submit %s News', $product['Product']['name']);
-		$this->set(compact('product', 'title_for_layout'));
-	}
-	
+class NewsController extends AppController {
+
+    
+    public function index($productSlug) {
+        $product = $this->News->Product->getBySlug($productSlug);
+
+        if (!$product) {
+            throw new NotFoundException(__('Invalid Product'));
+        }
+    }
+
+    public function view($slug) {
+
+        $news = $this->News->find('first', array(
+            'conditions' => array(
+                'News.slug' => $slug,
+                'News.deleted' => 0,
+                'News.published' => 1
+            ),
+            'contain' => array(
+                'User' => array('username', 'slug')
+            )
+                ));
+
+        if (!$news) {
+            throw new NotFoundException('Invalid News Item');
+        }
+
+        $this->set('news', $news);
+
+        // Get product
+        $this->set('product', $this->News->Product->getById($news['News']['product_id']));
+
+        $this->set('title_for_layout', $news['News']['title']);
+    }
+
+    public function submit($productSlug) {
+        $product = $this->News->Product->getBySlug($productSlug);
+
+        if (!$product) {
+            throw new NotFoundException(__('Invalid Product'));
+        }
+
+        if ($this->request->is('post')) {
+
+            $this->request->data['News']['product_id'] = $product['Product']['id'];
+
+            try {
+                $news = $this->News->submit($this->request->data, AuthComponent::user('id'));
+            } catch (Exception $e) {
+                $this->setFlash($e->getMessage);
+            }
+
+            if ($news !== false) {
+                $this->Session->setFlash(__('Thanks for submiting your %s news.', htmlspecialchars($product['Product']['name'])));
+                $this->redirect(array('controller' => 'news', 'action' => 'index', 'productSlug' => $productSlug));
+            }
+        }
+
+        $this->set('title_for_layout', __('Submit %s News', $product['Product']['name']));
+        $this->set('product', $product);
+    }
+
 }
--- Model/News.php
+++ Model/News.php
@@ -2,87 +2,78 @@
 
 class News extends AppModel {
 
-	public $belongsTo = array(
-		'Product' => array('counterCache' => array('News.published' => true, 'News.deleted' => false)),
-		'User' => array('counterCache' => array('News.published' => true, 'News.deleted' => false))
-	);
-	
-	public $actsAs = array(
-		'Utils.Sluggable' => array(
-			'label' => 'title',
-			'method' => 'multibyteSlug',
-			'separator' => '-'
-		)
-	);
-	
-	public $validate = array(
-		'title' => array(
-			'required' => true,
-			'rule' => array('minLength', '10'),
-			'message' => 'The title must be at least 10 characters long.'
-		),
-		'content_raw' => array(
-			'required' => true,
-			'allowEmpty' => false,
-			'rule' => array('minLength', '10'),
-			'message' => 'Please post some content.'
-		)
-	);
-	
-	/**
-	* Fetches a users timeline
-	* @param $products array of product IDs
-	* @return array News
-	**/
-	public function timeline($products = array(), $limit = 20 ) {
-		return $this->find('all', array(
-			'conditions' => array(
-				'product_id' => $products,
-				'News.published' => 1,
-				'News.deleted' => 0
-			), 
-			'order' => array('News.created DESC'),
-			'limit' => $limit
-		));
-	}
-	
-	/**
-	 * Handles User submitted news.
-	 */
-	public function submit($newsData, $userId) {
-		// Check Product Exists
-		$this->Product->id = $newsData['News']['product_id'];
-		if(!$this->Product->exists()) {
-			throw new DomainException(__('Invalid product.'));
-		}
-		
-		// Check User Exists
-		$this->User->id = $userId;
-		if(!$this->User->exists()) {
-			throw new DomainException(__('Invalid User.'));
-		}
-		
-		$newsData['News']['user_id'] = $userId;
-		$newsData['News']['deleted'] = 0;
-		
-		// @todo Logic to see if post is auto-published
-		$newsData['News']['published'] = 1;
-		
-		return $this->add($newsData);
-	}
-	
-	/**
-	 * Adds a news item
-	 */
-	public function add($newsData) {
-		$this->create();
-		
-		// @todo add formatting
-		$newsData['News']['content'] = htmlspecialchars($newsData['News']['content_raw']);
-                
-                $this->save($newsData);
-		
-		return $this->read();
-	}
-	
+    public $belongsTo = array(
+        'Product' => array('counterCache' => array('News.published' => true, 'News.deleted' => false)),
+        'User' => array('counterCache' => array('News.published' => true, 'News.deleted' => false))
+    );
+    public $actsAs = array(
+        'Utils.Sluggable' => array(
+            'label' => 'title',
+            'method' => 'multibyteSlug',
+            'separator' => '-'
+        )
+    );
+    public $validate = array(
+        'title' => array(
+            'required' => true,
+            'rule' => array('minLength', '10'),
+            'message' => 'The title must be at least 10 characters long.'
+        ),
+        'content_raw' => array(
+            'required' => true,
+            'allowEmpty' => false,
+            'rule' => array('minLength', '10'),
+            'message' => 'Please post some content.'
+        )
+    );
+
+    /**
+     * Fetches a users timeline
+     * @param $products array of product IDs
+     * @return array News
+     * */
+    public function timeline($products = array(), $limit = 20) {
+        return $this->find('all', array(
+                    'conditions' => array(
+                        'product_id' => $products,
+                        'News.published' => 1,
+                        'News.deleted' => 0
+                    ),
+                    'order' => array('News.created DESC'),
+                    'limit' => $limit
+                ));
+    }
+
+    /**
+     * Handles User submitted news.
+     */
+    public function submit($newsData, $userId) {
+        // Check Product Exists
+        $this->Product->id = $newsData['News']['product_id'];
+        if (!$this->Product->exists()) {
+            throw new DomainException(__('Invalid product.'));
+        }
+
+        // Check User Exists
+        $this->User->id = $userId;
+        if (!$this->User->exists()) {
+            throw new DomainException(__('Invalid User.'));
+        }
+
+        $newsData['News']['user_id'] = $userId;
+        $newsData['News']['deleted'] = 0;
+
+        // @todo Logic to see if post is auto-published
+        if(AuthComponent::User('admin') == 1) {
+            $newsData['News']['published'] = 1;
+        }
+
+        // @todo add formatting
+        $newsData['News']['content'] = htmlspecialchars($newsData['News']['content_raw']);
+
+        // Save teh news
+        $this->create();
+
+        return $this->save($newsData);
+    }
 }
