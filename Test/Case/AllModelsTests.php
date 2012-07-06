c9748d8c93a2bde6f21ad9da47dbcdeabccf0c6b
Matthew Dunham <me@dunhamzzz.com>
2012-07-06 15:45:42 +0100

    Adding test suite for all models

---
+++ Test/Case/AllModelsTests.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Custom test suite to execute all tests
+ */
+
+class AllModelsTests extends PHPUnit_Framework_TestSuite {
+    
+    public static function suite() {
+        
+        $path = APP . 'Test' . DS . 'Case' . DS;
+        
+        $suite = new CakeTestSuite('All tests');
+        $suite->addTestDirectory($path . 'Model' . DS);
+        return $suite;
+        
+    }
+    
+}
