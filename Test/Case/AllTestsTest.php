c11fa5d747bafd697401c827ba0c8e5b5600dca1
Matthew Dunham <me@dunhamzzz.com>
2012-09-07 11:09:43 +0100

    Creating AllTests test case and adding controller test

---
+++ Test/Case/AllTestsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Custom test suite to execute all tests
+ */
+
+class AllTestsTest extends PHPUnit_Framework_TestSuite {
+
+    public static function suite() {
+
+        $path = APP . 'Test' . DS . 'Case' . DS;
+
+        $suite = new CakeTestSuite('All tests');
+        $suite->addTestDirectory($path . 'Model' . DS);
+        $suite->addTestDirectory($path . 'Controller' . DS);
+        return $suite;
+
+    }
+
+}
